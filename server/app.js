2aab80990bf90d48cf88a12b7c0fe4d744cdef4d
yamoo9 <yamoo9@naver.com>
2022-10-25 15:46:42 +0900

    todo. getArgv 유틸리티 함수 작성하기

--- server/app.js
+++ server/app.js
@@ -1,1 +1,21 @@
-console.log(process.env.LANG);
+
+const { join, resolve } = require('node:path');
+const liveServer = require('live-server');
+
+/* argv --------------------------------------------------------------------- */
+console.log(argv); // process.argv
+
+/* path.join() vs. path.resolve() ------------------------------------------- */
+
+// console.log(join(process.cwd(), 'public/scripts/app.js'));
+// console.log(resolve('public/scripts/app.js'));
+
+
+// const params = {
+//   host: 'localhost',
+//   port: process.env.PORT ?? 3000,
+//   open: false,
+//   root: resolve('public')
+// }
+
+// liveServer.start(params);
