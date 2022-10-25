401faedba54d1d02f197e70f3e462bfeabca1ef8
yamoo9 <yamoo9@naver.com>
2022-10-25 16:26:56 +0900

    config. 서버 구동
    - getArgv 유틸리티 함수
    - NPM 스크립트 수정

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
-
 const { join, resolve } = require('node:path');
 const liveServer = require('live-server');
+const getArgv = require('./helpers/getArgv');
 
-/* argv --------------------------------------------------------------------- */
-console.log(argv); // process.argv
+const defaultParams = {
+  host: 'localhost',
+  port: 3000,
+  root: './public',
+  open: false,
+};
+
+const userParams = getArgv();
+
+const params = Object.assign({}, defaultParams, userParams);
+
+liveServer.start(params);
 
 /* path.join() vs. path.resolve() ------------------------------------------- */
 
 // console.log(join(process.cwd(), 'public/scripts/app.js'));
 // console.log(resolve('public/scripts/app.js'));
-
-
-// const params = {
-//   host: 'localhost',
-//   port: process.env.PORT ?? 3000,
-//   open: false,
-//   root: resolve('public')
-// }
-
-// liveServer.start(params);
--- server/helpers/getArgv.js
+++ server/helpers/getArgv.js
@@ -1,14 +1,21 @@
 // --------------------------------------------------------------------------
-// TODO                                                                      
+// TODO
 // --------------------------------------------------------------------------
-// - [ ] process.argv 배열에서 사용자가 전달한 인자를 가져오기
-// - [ ] 인자의 구조를 분석해서 key:value 쌍 값의 데이터 구조화 구성 후, 반환
+// - [x] process.argv 배열에서 사용자가 전달한 인자를 가져오기
+// - [x] 인자의 구조를 분석해서 key:value 쌍 값의 데이터 구조화 구성 후, 반환
 // --------------------------------------------------------------------------
 
 const { argv } = require('node:process');
+const SEPERATOR = '=';
 
-const getArgv = () => {
-  // return { key: value, ... }
-}
+const getArgv = () =>
+  Object.fromEntries(
+    argv.slice(2).map((keyValue) => {
+      keyValue = keyValue.replace(/--/g, '');
+      return keyValue.includes(SEPERATOR)
+        ? keyValue.split(SEPERATOR)
+        : [keyValue, true];
+    })
+  );
 
 module.exports = getArgv;
