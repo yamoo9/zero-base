5632678ba79fd33f0a586618f671bf73b733be27
yamoo9 <yamoo9@naver.com>
2022-10-26 15:37:17 +0900

    test. Node.js 런타임 환경에서의 Jest 테스트
    - require()
    - module.exports

--- public/utils/getRandom.js
+++ public/utils/getRandom.js
@@ -1,1 +1,4 @@
-exports.getRandom = () => null;
+const getRandom = (n) => Math.round(Math.random() * n);
+const getRandomMinMax = (min = 0, max = 100) => getRandom(max - min) + min;
+
+module.exports = { getRandom, getRandomMinMax };
--- public/utils/getRandom.test.js
+++ public/utils/getRandom.test.js
@@ -1,8 +1,17 @@
-const { getRandom } = require('./getRandom');
+const { getRandom, getRandomMinMax } = require('./getRandom');
 
 test('getRandom(10)의 반환 값은 0보다 크거나 같고, 10보다 작거나 같다.', () => {
-  let value = 10;
-  let result = getRandom(value);
-  expect(result).toBeGreaterThan(value);
-  expect(result).toBeLessThanOrEqual(value);
+  let min = 0;
+  let max = 10;
+  let result = getRandom(max);
+  expect(result).toBeGreaterThanOrEqual(min);
+  expect(result).toBeLessThanOrEqual(max);
+});
+
+test('getRandomMinMax(22, 46)의 반환 값은 22보다 크거나 같고, 46보다 작거나 같다.', () => {
+  let min = 22;
+  let max = 46;
+  let result = getRandomMinMax(min, max);
+  expect(result).toBeGreaterThanOrEqual(min);
+  expect(result).toBeLessThanOrEqual(max);
 });
