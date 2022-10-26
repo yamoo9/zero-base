99a109771b164a6882be063eaeae2ab453517306
yamoo9 <yamoo9@naver.com>
2022-10-26 15:27:57 +0900

    config. Jest 테스트 러너

--- public/utils/getRandom.test.js
+++ public/utils/getRandom.test.js
@@ -1,5 +1,8 @@
-import { getRandom } from './getRandom.js';
+const { getRandom } = require('./getRandom');
 
 test('getRandom(10)의 반환 값은 0보다 크거나 같고, 10보다 작거나 같다.', () => {
-  expect(getRandom(10)).toBeLessThanOrEqual(10);
+  let value = 10;
+  let result = getRandom(value);
+  expect(result).toBeGreaterThan(value);
+  expect(result).toBeLessThanOrEqual(value);
 });
