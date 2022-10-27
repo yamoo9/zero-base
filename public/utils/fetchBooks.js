1ef22cf848d4ffdc47cafe6c6fd6c82e13ea0725
yamoo9 <yamoo9@naver.com>
2022-10-27 17:25:49 +0900

    config. Babel 플러그인 구성

--- public/utils/fetchBooks.js
+++ public/utils/fetchBooks.js
@@ -1,10 +1,6 @@
 // `/api/books.json` 모듈을 불러와 booksApi 이름으로 사용합니다.
 // import('../api/books.json', { assert: { type: 'json' } });
-// import booksApi from '../api/books.json' assert { type: 'json' };
-import booksApi from '../api/books.json';
-
-// 1. XMLHttpRequest
-// 2. fetch API
+import booksApi from '../api/books.json' assert { type: 'json' };
 
 export const fetchBooks = (timeout = 1000, shouldReject = false) => {
   return new Promise((resolve, reject) => {
