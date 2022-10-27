d0163c41d07115477dba1ae817a7f64baa27b29a
yamoo9 <yamoo9@naver.com>
2022-10-27 13:47:14 +0900

    learn. 태그 템플릿 구문을 활용한 스타일 유틸리티
    - aka. styled-components

--- public/scripts/__tests__/02.2-tagged-template-literals.js
+++ public/scripts/__tests__/02.2-tagged-template-literals.js
@@ -1,8 +1,10 @@
+/* eslint-disable jest/no-focused-tests */
 // -----------------------------------------------------------------------------
 // TODO
 // -----------------------------------------------------------------------------
 // - [ ] styled, $styled 함수를 작성합니다.
 // - [ ] 태그 템플릿(Tagged Template) 구문을 활용해봅니다.
+// - [ ] 커링 함수를 활용해 사용의 편의성을 높여봅니다.
 // -----------------------------------------------------------------------------
 
 const virtualNode = {
@@ -11,49 +13,59 @@ const virtualNode = {
   style: { cssText: '' },
 };
 
-const styled = () => {
-  // 코드 로직을 작성합니다.
+const styled = (styleList, node) => {
+  node.style.cssText = styleList.reduce(
+    (styleValue, currentStyleValue) => styleValue + currentStyleValue.trim(),
+    ''
+  );
+  return node;
 };
 
-const $styled = () => {
-  // 코드 로직을 작성합니다.
+const $styled = (node) => (styleRules) => {
+  node.style.cssText = styleRules.reduce(
+    (styleValue, currentStyleValue) => styleValue + currentStyleValue.trim(),
+    ''
+  );
+  return node;
 };
-
 
 // ------------------------------------------------------------------------------
-// TEST                                                                      
+// TEST
 // ------------------------------------------------------------------------------
 // - [ ] Jest 테스트 러너를 구동한 후, 테스트가 성공하도록 함수 로직을 구성합니다.
 // ------------------------------------------------------------------------------
 
-// describe('styled 유틸리티 suite', () => {
-//   let values = {
-//     margin: '10px',
-//     color: '#d08471'
-//   };
+describe('styled 유틸리티 suite', () => {
+  let values = {
+    margin: '10px',
+    color: '#d08471',
+  };
 
-//   test('styled 유틸리티 ← 일반 함수 구문 활용', () => {
-//     const received = styled(['margin:10px;', `color:${values.color};`], virtualNode);
+  test('styled 유틸리티 ← 일반 함수 구문 활용', () => {
+    const received = styled(
+      [`margin:${values.margin};`, `color:${values.color};`],
+      virtualNode
+    );
 
-//     expect(received.style.cssText).toBe(virtualNode.style.cssText);
-//   });
+    expect(received.style.cssText).toBe(virtualNode.style.cssText);
+  });
 
-//   test('styled 유틸리티 ← 태그 템플릿 구문 활용', () => {
-//     const received = styled`
-//       ${virtualNode}
-//       margin: ${values.margin};
-//       color: ${values.color};
-//     `;
+  test('styled 유틸리티 ← 태그 템플릿 구문 활용', () => {
+    const received = styled`
+      ${virtualNode}
+      margin: ${values.margin};
+      color: ${values.color};
+    `;
 
-//     expect(received.style.cssText).toBe(virtualNode.style.cssText);
-//   });
+    expect(received.style.cssText).toBe(virtualNode.style.cssText);
+  });
 
-//   test('$styled 유틸리티 ← 커링 + 태그 템플릿 구문 활용', () => {
-//     const received = $styled(virtualNode)`
-//       margin: ${values.margin};
-//       color: ${values.color};
-//     `;
+  test('$styled 유틸리티 ← 커링 + 태그 템플릿 구문 활용', () => {
+    const received = $styled(virtualNode)`
+      margin: ${values.margin};
+      color: ${values.color};
+    `;
 
-//     expect(received).toStrictEqual(virtualNode);
-//   });
-// });
+    expect(received).toStrictEqual(virtualNode);
+  });
+});
