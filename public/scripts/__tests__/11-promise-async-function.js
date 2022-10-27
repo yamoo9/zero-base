5cf75ca2ccc7d1ff01ace54605b8abc75437391a
yamoo9 <yamoo9@naver.com>
2022-10-27 16:30:34 +0900

    learn. Promise, Async Function 테스트

--- public/scripts/__tests__/11-promise-async-function.js
+++ public/scripts/__tests__/11-promise-async-function.js
@@ -9,15 +9,28 @@
 // - [ ] Jest 테스트 러너를 구동한 후, 테스트가 성공하도록 함수 로직을 구성합니다.
 // -----------------------------------------------------------------------------
 
+import { fetchBooks } from '../../utils/index.js';
 
-// 데이터 패치 성공 시뮬레이션 코드 예시 
+// 데이터 패치 성공 시뮬레이션 코드 예시
 test('fetchBooks Promise 테스트', () => {
   // Promise를 사용해 fetchBooks()로 부터 가져온 데이터를 테스트 합니다.
-  // 통신에 실패한 경우, 출력된 오류 메시지를 테스트 합니다.
+  return fetchBooks()
+    .then((data) => {
+      expect(data).toHaveLength(7);
+    })
+    .catch((error) => {
+      expect(error.message).toMatch(/unknown/i);
+    });
 });
 
 // 데이터 패치 실패 시뮬레이션 코드 예시
-test('fetchBooks Async 함수 테스트', () => {
+test('fetchBooks Async 함수 테스트', async () => {
   // Async 함수를 사용해 fetchBooks()로 부터 가져온 데이터를 테스트 합니다.
-  // 통신에 실패한 경우, 오류 메시지를 테스트 합니다.
+  try {
+    const books = await fetchBooks();
+    expect(books).toHaveLength(7);
+  } catch (error) {
+    // 통신에 실패한 경우, 오류 메시지를 테스트 합니다.
+    expect(error.message).toMatch(/unknown/i);
+  }
 });
--- public/utils/fetchBooks.js
+++ public/utils/fetchBooks.js
@@ -1,18 +1,18 @@
 // `/api/books.json` 모듈을 불러와 booksApi 이름으로 사용합니다.
 // import('../api/books.json', { assert: { type: 'json' } });
-import booksApi from '../api/books.json' assert { type: 'json' };
+// import booksApi from '../api/books.json' assert { type: 'json' };
+import booksApi from '../api/books.json';
+
+// 1. XMLHttpRequest
+// 2. fetch API
 
 export const fetchBooks = (timeout = 1000, shouldReject = false) => {
-  // Promise 객체를 생성 반환합니다.
   return new Promise((resolve, reject) => {
-    // timeout 초가 흐른 뒤, Promise.resolve, Promise.reject가 작동하도록 설정합니다.
     setTimeout(() => {
-      // shouldReject 값에 따라 오류 객체를 전송하거나,
-      if (!shouldReject) {
-        // booksApi.data 값을 전송합니다.
-        resolve(booksApi.data);
-      } else {
+      if (shouldReject) {
         reject({ message: 'UNKNOWN ERROR' });
+      } else {
+        resolve(booksApi.data);
       }
     }, timeout);
   });
