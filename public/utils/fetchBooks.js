3c51302aa7d31a09c0468275c97f9df9d8f045d0
yamoo9 <yamoo9@naver.com>
2022-10-27 16:05:38 +0900

    config. ESLint assertions 오류 문제 해결

--- public/utils/fetchBooks.js
+++ public/utils/fetchBooks.js
@@ -1,4 +1,5 @@
 // `/api/books.json` 모듈을 불러와 booksApi 이름으로 사용합니다.
+// import('../api/books.json', { assert: { type: 'json' } });
 import booksApi from '../api/books.json' assert { type: 'json' };
 
 export const fetchBooks = (timeout = 1000, shouldReject = false) => {
