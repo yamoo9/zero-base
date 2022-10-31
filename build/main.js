de1d7babc155c86ab3cfab6bb86ee4dc252071cf
yamoo9 <yamoo9@naver.com>
2022-10-31 17:31:15 +0900

    learn. 명령어 추가
    - npm-run-all
    - build command

---
+++ build/main.js
@@ -0,0 +1,1 @@
+import{App}from"./components/App.js";var container=document.getElementById("react-root");var ReactDomRoot=ReactDOM.createRoot(container);ReactDomRoot.render(React.createElement(React.StrictMode,null,React.createElement(App,null)));
