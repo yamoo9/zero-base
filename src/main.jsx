51df974ff40ed7476c460e6c5bf4ef70876910c5
yamoo9 <yamoo9@naver.com>
2022-11-04 15:13:33 +0900

    config. 환경 변수 재구성
    - 개발용 : .env/.dev
    - 배포용 : .env/.prod

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import '@/styles/global.css';
 import App from '@/app/App';
 
-console.log(process.env.API_TOKEN);
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
