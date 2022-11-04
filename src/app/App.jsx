d695839ba1979c4e00699f215d60ccd95f67e507
yamoo9 <yamoo9@naver.com>
2022-11-04 17:52:29 +0900

    learn. 리스트 렌더링
    - 배열 리스트 렌더링

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,7 +6,7 @@ import { EmojiOops, PrettyPrintCode } from '@/components';
 /* -------------------------------------------------------------------------- */
 
 export default function App() {
-  return <ConditionalRendering />;
+  return <ListRedering />;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -17,42 +17,52 @@ let error = null;
 error = {
   title: 'UNKNOWN ERROR',
   message: '알 수 없는 오류가 발생했습니다.',
+  print() {
+    console.error(`${this.title} ${this.message}`);
+  },
 };
 
 /* -------------------------------------------------------------------------- */
 
 const ConditionalRendering = () => {
-  if (error === null || error === undefined) {
-    console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.');
-  }
+  error ?? console.log('현재 앱에는 오류(error)가 발생하지 않았습니다.');
 
-  error && typeof error.log === 'function' && error.log();
-
-  // JSX 조건부 렌더링: if 문
-  // 오류가 존재하면 렌더링 되도록 코드를 작성합니다.
-  if (error) {
-    const displayErrorState = (
-      <div className={styles.container}>
-        <h1 className={styles.headline}>
-          <EmojiOops height={200} />
-          {/* 오류 메시지가 출력되도록 코드를 변경합니다. */}
-          {error.message}
-        </h1>
-      </div>
-    );
-
-    return displayErrorState;
-  }
+  error?.log?.();
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.headline}>React 조건부 렌더링</h1>
-      <p>오류가 존재하면 렌더링 되도록 코드를 작성합니다.</p>
+      <h1 className={styles.headline}>
+        {!error
+          ? 'React 조건부 렌더링'
+          : [<EmojiOops key="EmojiOops" height={200} />, error.message]}
+      </h1>
+      {!error && <p>오류가 존재하면 렌더링 되도록 코드를 작성합니다.</p>}
     </div>
   );
 };
 
 /* -------------------------------------------------------------------------- */
+
+// import db from '../api/db.json';
+// import('../api/db.json').then(({ navigation }) => console.log(navigation));
+
+const db = require('../api/db.json');
+
+const {
+  navigation: { items: list },
+} = db;
+
+const ListItem = ({ link, text }) => (
+  <li>
+    <a href={link}>{text}</a>
+  </li>
+);
+
+const renderList = (list) => {
+  return list.map(({ link, text }) => (
+    <ListItem key={link} link={link} text={text} />
+  ));
+};
 
 // `api/db.json` 데이터에서 `navigation.items` 데이터를 화면에 출력해보세요.
 // `list` 매개 변수를 순환해 아이템 리스트를 반환하는 `renderList` 함수를 만들어 활용해봅니다.
@@ -66,9 +76,7 @@ const ListRedering = () => {
       <nav className={styles.globalNavigation}>
         {/* 웹 표준을 준수해 비순차 목록을 화면에 출력해봅니다. */}
         {/* renderList(list) 함수를 실행해 리스트 렌더링 처리해봅니다. */}
-        <ul>
-          <li>리스트를 렌더링 해보세요.</li>
-        </ul>
+        <ul>{renderList(list)}</ul>
       </nav>
 
       {/* 객체 리스트 렌더링 */}
