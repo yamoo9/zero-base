0b6c9c87209d9f1d9d4e6e329e308d0841b4180b
yamoo9 <yamoo9@naver.com>
2022-11-07 16:19:34 +0900

    add. public 에셋

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,33 +1,5 @@
-// import './App.scss';
 import styles from './App.module.scss';
-import logo from 'assets/logo.svg';
-import { getAsset } from 'utils';
-import { Demo } from 'components';
 
 export default function App() {
-  return (
-    <div className={styles.App}>
-      <Demo />
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="React 로고" />
-        <img
-          src={getAsset('logo512.png')}
-          className={styles.AppLogo}
-          alt="React 로고"
-        />
-        <p>
-          <code>src/app/App.jsx</code> 파일을 수정하면 브라우저가
-          새로고침됩니다.
-        </p>
-        <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noreferrer"
-        >
-          React 배우기
-        </a>
-      </header>
-    </div>
-  );
+  return <div className={styles.App} />;
 }
