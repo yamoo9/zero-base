983c64ab285d4a9f289f8e0febdfea8e011bb76e
yamoo9 <yamoo9@naver.com>
2022-11-08 13:24:59 +0900

    learn. SVGR 컴포넌트
    - CRA 툴체인 (SVGR 구성 프리셋)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,28 @@
 import styles from './App.module.scss';
 import { ToggleButton } from 'components';
+import reactLogoUrl, { ReactComponent as ReactLogo } from 'assets/logo.svg';
 
 export default function App() {
   return (
     <div className={styles.App}>
-      <ToggleButton
-        onText="ON"
-        offText="OFF"
-        onToggle={() => {
-          console.log('hi toggle button');
-        }}
-      />
-      <ToggleButton onText="ON" offText="OFF" on />
+      <a className="homelink" href="/">
+        <img src={reactLogoUrl} alt="React 로고" />
+      </a>
+
+      <figure>
+        <ReactLogo title="React 로고 SVG" data-id="svg-logo" />
+      </figure>
+
+      <div role="group">
+        <ToggleButton
+          onText="ON"
+          offText="OFF"
+          onToggle={() => {
+            console.log('hi toggle button');
+          }}
+        />
+        <ToggleButton onText="ON" offText="OFF" on />
+      </div>
     </div>
   );
 }
